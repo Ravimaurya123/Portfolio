87e66e395849e27fce2cd45408203a5df5b8b0ac
Ravimaurya123 <ravikantsingh08032007@gmail.com>
2026-08-25 23:44:18 +0530

    Footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,8 @@ export default function Footer() {
   return (
     <footer className="border-t border-white/10 px-6 py-8">
       <div className="mx-auto max-w-7xl text-center text-sm text-gray-500">
+        console.log("Thanks For Visiting My Portfolio!");
+        
         © {new Date().getFullYear()} Ravikant Singh. All rights reserved.
       </div>
     </footer>
